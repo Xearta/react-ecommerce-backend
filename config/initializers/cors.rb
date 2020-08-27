2a90c8776fdb10e706831244663f327c9ee01138
Xearta <rsmcwow@gmail.com>
2020-08-27 15:41:55 -0400

    Updated cors for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://react-widget-store.herokuapp.com/'
 
     resource '*',
       headers: :any,
